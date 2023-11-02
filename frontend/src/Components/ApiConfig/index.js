98a4e7c99aeb9ce3de38da2883b99cf03431f1d8
pooh-14 <poojavetal14@gmail.com>
2023-11-02 23:49:53 +0530

    done

--- frontend/src/Components/ApiConfig/index.js
+++ frontend/src/Components/ApiConfig/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = JSON.parse(localStorage.getItem("token"));
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://admin-instructor-panel-rha5.onrender.com",
   headers: { Authorization: `Bearer ${token}` }
 });
 
